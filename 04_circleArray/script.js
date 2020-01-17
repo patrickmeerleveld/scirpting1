28dcd7dca786c2a733141c4dfe7f9eb4c702e3f7
patrickmeerleveld <patrick@meerleveld.com>
2020-01-17 12:01:55 +0100

    Opdracht 4

--- 04_circleArray/script.js
+++ 04_circleArray/script.js
@@ -9,26 +9,28 @@ const width = window.innerWidth;
 const height = window.innerHeight;
 canvas.width = width;
 canvas.height = height;
+var color = '#' + Math.floor(Math.random() * 16777215).toString(16);
+
+let colors = ["RGBA(0,0,255,1)", "RGBA(0,255,0,1)", "RGBA(255,255,255,1)"];
 
 circles = [];
-
-for(let i =0; i<10 ;i++){
-  let circleObject = {};
+for (let i = 0; i < 100; i++){
+let circleObject = {};
   circleObject.x = getRandomInt(0,width);
   circleObject.y = getRandomInt(0,height);
   circleObject.radius = 40;
-  circleObject.color = "purple";
+  circleObject.color
   circleObject.speedX = 6;
   circleObject.speedY = 7;
 
   circleObject.draw = function(){
     context.beginPath();
     context.lineWidth = "5";
-    context.fillStyle = circleObject.color;
+    context.fillStyle = colors[getRandomNumber(colors.length)];
     context.arc(circleObject.x,circleObject.y,circleObject.radius,0,2*Math.PI);
-    context.closePath();
     context.stroke();
     context.fill();
+    context.closePath();
   }
   circleObject.update = function(){
     circleObject.x = circleObject.x + circleObject.speedX;
@@ -50,22 +52,27 @@ for(let i =0; i<10 ;i++){
       circleObject.speedX =  -circleObject.speedX
     }
   }
-
   circles.push(circleObject);
+}
 
+
+
+function getRandomInt(min,max)
+{
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+function getRandomNumber(max){
+    return Math.floor(Math.random()*max);
 }
 
 function anime(){
   context.clearRect(0,0,width,height);
-  circleObject.update();
-  circleObject.draw();
+  for (let i = 0; i<circles.length;i++){
+    circles[i].update();
+    circles[i].draw();
+  }
 }
-
-//setInterval(anime,10)
-
-
-function getRandomInt(min,max){
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random()*(max-min+1) + min);
-}
+setInterval(anime,10)
